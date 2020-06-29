eeeb355991e6d9ea6a9c38058fb1e161431e8214
aakashDeveloper <ahanda205@gmail.com>
2020-06-29 20:55:13 +0530

    a

---
+++ inbuiltpackage/fs.js
@@ -0,0 +1,28 @@
+var fs = require('fs');
+
+/*fs.writeFile('mytest.txt','This is from nodeFs nareshit',function(err){
+    if(err) throw err;
+    console.log('File Crreated')
+})
+
+fs.appendFile('mytest.txt',Math.floor(Math.random()*(20-1))+1+' This is from nodeFs nareshit\n',function(err){
+    if(err) throw err;
+    console.log('File Crreated')
+})
+
+fs.readFile('db.json','utf-8',function(err,data){
+    if(err) throw err;
+    console.log(data)
+})
+
+
+fs.rename('mytest.txt','myText.txt',function(err){
+    if(err) throw err;
+    console.log("File remaned")
+})
+
+
+fs.unlink('myText.txt',function(err){
+    if(err) throw err;
+    console.log("File deleted")
+})*/
