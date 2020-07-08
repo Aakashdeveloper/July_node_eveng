30ecd0f37bcf838d4a1ca891f5f4d131ffdc28f6
ahand14 <aakash.handa@aexp.com>
2020-07-08 20:44:46 +0530

    a

---
+++ weatherapp/server.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+var port = 8999;
+
+//Static File path
+app.use(express.static(__dirname+'/public'));
+//html
+app.set('views','./src/views');
+//view engine
+app.set('view engine','ejs');
+
+var apiUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Mumbai&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29"
+
+
+app.get('/weather',(req,res)=>{
+    request(apiUrl,(err,response)=>{
+        if(err) throw err;
+        //res.send(response.body)
+        var output = JSON.parse(response.body)
+        res.render('index',{title:'Weather App',result:output})
+    })
+})
+
+app.listen(port,(err)=>{
+    if(err) throw err;
+    console.log("Server is running on port "+port)
+})
