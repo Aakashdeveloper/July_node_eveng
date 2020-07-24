9398cdf8cb7eb11cf8ec2f0815860b3804e9b7af
aakashdeveloper <ahanda205@gmail.com>
2020-07-24 20:51:58 +0530

    A

---
+++ graphqlapp/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+//const expressGraphQL = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql');
+const port = 8600;
+const app = express();
+const cors = require('cors');
+var schema = require('./schema/schema');
+
+app.use(cors());
+
+app.use(
+    '/graphql',
+    graphqlHTTP({
+      schema: schema,
+      graphiql: true,
+    }),
+  );
+
+  
+app.listen(port,() => {
+    console.log(`App is running on port ${port}`)
+})
+
+/*
+app.use('/graphql', expressGraphQL({
+    schema,
+    graphiql:true
+}))*/
